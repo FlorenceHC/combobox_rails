8a35710c504a4f2df1da876c7e8d11e04fd6f396
Jesse Bailey <dev@jsilasbailey.com>
2024-03-28 15:25:39 -0400

    Remove conflicting controllers with host app
    Currently, while porting out functionality from the previous host app,
    there are conflicts between stimulus controllers that are defined in the
    host app and that the SelectComponent depends on as well. We don't keep
    running both of these as then duplicate controllers will be running at
    the same time (as we have namespaced the combobox-controllers) and the
    behavior of the application could conflict.

--- app/javascript/combobox-controllers/remove_if_js_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="remove-if-js"
-export default class extends Controller {
-  connect() {
-    this.element.remove();
-  }
-}
